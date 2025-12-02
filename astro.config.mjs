73594a651255a5df8acf4e32970c67f0d2471bbb
m <m@mail.com>
2025-12-02 07:21:50 +0100

    first page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
 });
